26b5030b77f8abcc9204b3e173c5935436c63ed7
Mariaki <47898516+Marina-Martin@users.noreply.github.com>
2024-05-28 13:28:44 -0300

    Simplificação do projeto
    agora ele funciona!

--- Projeto/Projeto.c
+++ Projeto/Projeto.c
@@ -1,50 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <omp.h>
-#include <math.h>
 
-void Taylor(int n, int fat, double* global_result_p);
+double fatorial(int n) {
+    double resultado = 1.0;
+    for (int i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
 
-int n = 5;
+int main() {
+    int N = 100000; // Número de termos da série de Taylor
+    double soma = 0.0;
 
-int main(int argc, char* argv[]){
-    double global_result = 0.0;
-    int thread_count;
-    int fat[n];
-    
-    thread_count = strtol(argv[1], NULL, 10);
-    printf("Enter n\n");
-    scanf("%d", &n);
-    #pragma omp parallel num_threads(thread_count)
-    Taylor(n, fat, &global_result);
-    
-    printf("With n = %d\n", n);
-    printf("our estimate for S is: %e\n", global_result);
+    #pragma omp parallel for reduction(+:soma)
+    for (int i = 0; i < N; i++) {
+        soma += 1.0 / fatorial(i);
+    }
+
+    printf("A soma da série de Taylor com %d termos é: %.10f\n", N, soma);
+
     return 0;
-}
-
-void Fat(int n, int fat){
-    fat[0] = 1;
-    fat[1] = 1;
-    
-    if(n > 1){
-        for(int i = 2; i < n; n++){
-            fat[n] += fat[n-1]
-        }
-    }
-}
-
-void Taylor(int n, int fat, double* global_result_p){
-    double my_result;
-    int i, local_n;
-    int my_rank = omp_get_thread_num();
-    int thread_count = omp_get_num_threads();
-    
-    for (i = 1; i <= local_n-1; i++){
-        my_result += 1 / Fat(local_n, fat);
-    }
-    
-    #pragma omp critical
-    *global_result_p += my_result;
-
 }
